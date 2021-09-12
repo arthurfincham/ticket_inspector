8a7c9c8a5cf2ada579b73b2034d47db5869a055a
Arthur Fincham <arthurfincham@me.com>
2021-09-12 14:35:36 +0100

    first commit

--- lib/ticket_inspector.rb
+++ lib/ticket_inspector.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
 require_relative "ticket_inspector/version"
+require 'excon'
+require 'json'
 
 module TicketInspector
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.say_hello_gem
+    "Hello, Gem!"
+  end
+
+  def self.call_api(url)
+    response = Excon.get(url)
+    return nil if response.status != 200
+    JSON.parse(response.body)
+  end
+
 end
